3d5551da6edf1fe7985ca5b99d4c730d843e5b40
Jose Ortega <jmortegafreire@gmail.com>
2024-11-18 11:59:07 -0300

    seting up environment

---
+++ src/main/java/com/jmortegaf/personal_expenses/PersonalExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.jmortegaf.personal_expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalExpensesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmortegaf/personal_expenses/PersonalExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmortegaf.personal_expenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
